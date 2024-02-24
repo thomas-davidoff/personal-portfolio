cee5d35fdac5a633a45b8fbb8456c19e570b9109
Thomas Davidoff <68920095+thomas-davidoff@users.noreply.github.com>
2024-02-24 14:16:03 -0500

    Updated Resume link

--- src/scenes/global/Appbar.jsx
+++ src/scenes/global/Appbar.jsx
@@ -169,7 +169,7 @@ export default function MyAppBar() {
             <ResumeButton
               variant="outlined"
               component="a"
-              href="https://personal-portfolio-pdf-server.s3.us-east-2.amazonaws.com/resume_2_26_23.pdf"
+              href="https://personal-portfolio-pdf-server.s3.us-east-2.amazonaws.com/resume_2_24_2024.docx"
               title="Click to go to my resume."
               target="_blank"
             >
